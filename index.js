e0c8ce57d39b011a15372f0bc06a268903c3412a
Richard Marquez <richard.marquez.2001@gmail.com>
2020-01-26 11:25:38 -0500

    t-60

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
-function myMove() {
-
-    var elem = document.getElementById("animate");
-
-    var pos = 400;
-
-    var id = setInterval(frame, 30);
-
-    function frame() {
-
-        if (pos === 0) {
-
-            clearInterval(id);
-
-        } else {
-
-            pos--;
-
-            elem.style.height = pos + "px";
-        }
-    }
-}
+let min = 1;
+let seconds = 1;
+let minNum = 0;
+let secondNum = 0;
+let questions = ["What is 3/5 + 2/6?", "What is 3/2 * 4/2?", "Divide 6 by 3"];
+let q = 0;
 
 function timerCnt(){
     let count = 0;
-    var x = setInterval(function(){
-        document.getElementById("timer").innerHTML= count.toString() + "s";
-        if(count >= 10) {
-            clearinterval(x);
+    let x = setInterval(function(){
+        minNum = Math.floor(count / 60);
+        secondNum = count - (minNum * 60);
+        if (minNum < 1){
+            document.getElementById("timer").innerHTML= count.toString() + "s";
+        }else{
+            document.getElementById("timer").innerHTML= minNum.toString() + "m " + secondNum.toString() + "s";
+        }
+
+        if(count >= ((min * 60) + seconds)) {
+            clearInterval(x);
             document.getElementById("timer").innerHTML = "EXPIRED";
+            $("html").load("break.html");
         }
         count += 1;
     },1000);
 }
 
-myMove();
+$("#question").html(questions[q].toString());
+
+$("#submit").click(function(e){
+
+    e.preventDefault();
+    console.log("reached");
+    q += 1;
+    if(q === questions.length){
+        $("html").load("break.html");
+        clearInterval(x);
+    }
+
+    $("#question").html(questions[q].toString());
+});
+
+$(".wrapper")
+
+    .css("display", "none")
+
+    .fadeIn(3000 , function(){
+        $(this).css("display","normal");
+    });
+
 timerCnt();
